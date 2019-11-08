4f33392ff8b3b6b22d7bd883060377aa952b531a
Leonel Ramos <Leonel Ramos@DESKTOP-RV9MRO8>
2019-11-07 22:50:21 -0600

    add image

--- app/src/main/java/com/example/calendaraplication/MainActivity.kt
+++ app/src/main/java/com/example/calendaraplication/MainActivity.kt
@@ -46,5 +46,6 @@ class MainActivity : AppCompatActivity() {
                 dpd.show()}
         }
 
+
     }
 }
--- app/src/main/java/com/example/calendaraplication/activity_principal.kt
+++ app/src/main/java/com/example/calendaraplication/activity_principal.kt
@@ -1,8 +1,12 @@
 package com.example.calendaraplication
 
+import android.app.DatePickerDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.DatePicker
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_principal.*
 import java.util.*
 
@@ -12,13 +16,39 @@ class activity_principal : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_principal)
 
-
         imageView2.setOnClickListener{ val Intento1 =Intent(this, MainActivity::class.java)
             this.finish()
             startActivity(Intento1)
         }
+
+        imageView4.setOnClickListener{
+            selectImageInAlbum()
+        }
+
+
+
     }
 
+
+    private fun selectImageInAlbum() { val intent = Intent(Intent.ACTION_GET_CONTENT)
+        intent.type = "image/*"
+        if (intent.resolveActivity(packageManager) != null)
+        {
+            startActivityForResult(intent, REQUEST_SELECT_IMAGE_IN_ALBUM)
+            val intent = Intent()
+                intent.type = "image/*"
+            intent.action = Intent.ACTION_GET_CONTENT
+            startActivityForResult(Intent.createChooser(intent, "Select Picture"), imageView3);
+        }
+    }
+    private fun takePhoto() {
+        val intent1 = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        if (intent1.resolveActivity(packageManager) != null)
+        {
+            startActivityForResult(intent1, REQUEST_TAKE_PHOTO)
+        }
+    }
+    companion object { private val REQUEST_TAKE_PHOTO = 0 ; private val REQUEST_SELECT_IMAGE_IN_ALBUM = 1 }
 
 
 
